43597497106a95db7c8b57aa24dcb39108e63354
Giorgio <tran.giorgio@gmail.com>
2023-11-29 16:43:35 -1000

    Test

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -1,14 +1,15 @@
-import { Selector } from 'testcafe';
+import { Selector } from "testcafe";
 
 class LandingPage {
   constructor() {
-    this.pageId = '#landing-page';
+    this.pageId = "#landing-page";
     this.pageSelector = Selector(this.pageId);
   }
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     await testController.expect(this.pageSelector.exists).ok();
+    // test 12345
   }
 }
 
